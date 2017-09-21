30a68084c8a391677b91fbfbbad116f33b7db717
ianboucher <i.a.boucher@gmail.com>
2017-09-21 07:13:54 +0100

    Add test for creation of Threads

---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CreateThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAnAuthenticatedUserCanCreateNewThreads()
+    {
+        $this->actingAs(factory('App\User')->create());
+        $thread = factory('App\Thread')->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
