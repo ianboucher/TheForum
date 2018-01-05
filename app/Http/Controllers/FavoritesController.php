0615534f6564efcbe711cdb537ffaacf89f27ee9
ianboucher <i.a.boucher@gmail.com>
2018-01-05 10:34:59 +0000

    Lesson 18 - User Can Favorite Replies

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -45,7 +45,7 @@ class FavoritesController extends Controller
      */
     public function store(Reply $reply)
     {
-        $reply->favorite();
+        return $reply->favorite(auth()->id());
     }
 
     /**
